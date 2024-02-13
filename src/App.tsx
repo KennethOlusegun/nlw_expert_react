3387c85be65e2cf25167328841c7329297d8bad4
KennethOlusegun <kennetholusegun@gmail>
2024-02-13 17:46:53 -0300

    feat: updating app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
-import Logo from './assets/Logo.svg'
-import NoteCard from "./compponents/note-card"
+import logo from "./assets/logo.svg";
+import { NewNoteCard } from "./compponents/new-note-card";
+import { NoteCard } from "./compponents/note-card";
 
 export function App() {
-
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
-		<img src={Logo} alt="Nlw Expert"/>
+      <img src={logo} alt="NLW Expert" />
 
-		<form>
-			<input
-			 type="text"
-			 placeholder="Busque suas notas..."
-			 className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"/>
-		</form>
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+        />
+      </form>
 
-		<div className="h-px bg-slate-700"/>
+      <div className="h-px bg-slate-700" />
 
-		<div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-			<div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden relative ">
-				<span className="text-sm font-medium text-slate-200">
-					Adicionar nota
-				</span>
-				<p className="text-sm leading-6 text-slate-400">
-					Grave uma nota em áudio que será convertido para texto automaticamente
-				</p>
-				
-			</div>
-
-
-			<NoteCard />
-			<NoteCard />
-			<NoteCard />
-		</div>
-	</div>
-  )
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  );
 }
