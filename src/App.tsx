ecc7ec5880127eeab07c918fe582823b3dd8a0f5
KennethOlusegun <kennetholusegun@gmail>
2024-02-13 10:44:16 -0300

    feat: creating input and logo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Logo from './assets/Logo.svg'
 
 export function App() {
 
   return (
-    <>
-		<h1>Hello World</h1>
-    </>
+    <div className="mx-auto max-w-6xl my-12">
+		<img src={Logo} alt="Nlw Expert"/>
+
+		<form>
+			<input
+			 type="text"
+			 placeholder="Busque suas notas..."
+			 className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"/>
+		</form>
+	</div>
   )
 }
