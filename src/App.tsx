bbe1127b3c924d0464eb34d7ed70a02536c03c9c
KennethOlusegun <kennetholusegun@gmail>
2024-02-25 14:28:19 -0300

    feat: create toaster and states functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import logo from "./assets/Logo.svg";
+import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./compponents/new-note-card";
 import { NoteCard } from "./compponents/note-card";
-
-const note = {
-	date: new Date(),
-	content:
-		"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum inventore facere fugiat amet debitis sint sapiente at nihil sequi nemo sed placeat rem eos minus perferendis voluptas, totam aspernatur eveniet!",
-};
-
 export function App() {
 	return (
 		<div className="mx-auto max-w-6xl my-12 space-y-6">
 			<img src={logo} alt="NLW Expert" />
-
 			<form className="w-full">
 				<input
 					type="text"
@@ -20,12 +12,16 @@ export function App() {
 					className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
 				/>
 			</form>
-
 			<div className="h-px bg-slate-700" />
 
 			<div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
 				<NewNoteCard />
-				<NoteCard note={note} />
+				<NoteCard
+					note={{
+						date: new Date(),
+						content: "Hello World",
+					}}
+				/>
 			</div>
 		</div>
 	);
--- src/compponents/note-card.tsx
+++ src/compponents/note-card.tsx
@@ -11,16 +11,13 @@ interface NoteCardProps {
 }
 
 export function NoteCard({ note }: NoteCardProps) {
-	if (!note) {
-		return null; // or return a fallback UI
-	}
-
 	return (
 		<Dialog.Root>
-			<Dialog.Trigger className="rounded-md text-left flex flex-col  bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+			<Dialog.Trigger className="rounded-md text-left bg-slate-800 flex flex-col p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
 				<span className="text-sm font-medium text-slate-300">
 					{note.date.toISOString()}
 				</span>
+
 				<p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
 				<div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
@@ -28,10 +25,11 @@ export function NoteCard({ note }: NoteCardProps) {
 
 			<Dialog.Portal>
 				<Dialog.Overlay className="inset-0 fixed bg-black/50" />
-				<Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-600 rounded-md flex flex-col outline-none">
+				<Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
 					<Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
 						<X className="size-5" />
 					</Dialog.Close>
+
 					<div className="flex flex-1 flex-col gap-3 p-5">
 						<span className="text-sm font-medium text-slate-300">
 							{formatDistanceToNow(note.date, {
@@ -49,7 +47,7 @@ export function NoteCard({ note }: NoteCardProps) {
 					>
 						Deseja{" "}
 						<span className="text-red-400 group-hover:underline">
-							apagar esta nota
+							apagar essa nota
 						</span>
 						?
 					</button>
