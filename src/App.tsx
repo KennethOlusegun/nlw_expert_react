4b8097c6bc01233649ce2c6ffaeff9730a6da660
KennethOlusegun <kennetholusegun@gmail>
2024-02-25 14:59:03 -0300

    feat: saving new notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./compponents/new-note-card";
 import { NoteCard } from "./compponents/note-card";
+import { useState } from "react";
+
+
 export function App() {
+	const [notes, setNotes] = useState([
+		{id: 1, date: new Date(), content: "Hello World"}
+	])
+
+	function onNoteCreated(content: string) {
+		const newNote = {
+			id: Math.random(),
+			date: new Date(),
+			content,
+		}
+
+		setNotes([newNote, ...notes ]);
+	}
+	
 	return (
 		<div className="mx-auto max-w-6xl my-12 space-y-6">
 			<img src={logo} alt="NLW Expert" />
@@ -15,13 +32,11 @@ export function App() {
 			<div className="h-px bg-slate-700" />
 
 			<div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-				<NewNoteCard />
-				<NoteCard
-					note={{
-						date: new Date(),
-						content: "Hello World",
-					}}
-				/>
+				<NewNoteCard onNoteCreated={onNoteCreated} />
+
+				{notes.map((note) => (
+					<NoteCard key={note.id} note={note} />
+				))}
 			</div>
 		</div>
 	);
