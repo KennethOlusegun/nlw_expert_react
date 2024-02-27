fdf72c188b71c16fb6bb2cacee7bd01e6bc4ab09
KennethOlusegun <kennetholusegun@gmail>
2024-02-26 21:52:48 -0300

    feat: functions to save queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./compponents/new-note-card";
 import { NoteCard } from "./compponents/note-card";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
+interface Note {
+	id: string;
+	date: Date;
+	content: string;
+}
 
 export function App() {
-	const [notes, setNotes] = useState([
-		{id: 1, date: new Date(), content: "Hello World"}
-	])
+	const [search, setSearch] = useState('')
+	const [notes, setNotes] = useState<Note[]>(() => {
+		const notesOnStorage = localStorage.getItem("notes");
+
+		if(notesOnStorage){
+			return JSON.parse(notesOnStorage);
+		}
+
+		return [];
+	});
+
+	function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+		const query = e.target.value
+		
+		setSearch(query)
+	}
 
 	function onNoteCreated(content: string) {
 		const newNote = {
-			id: Math.random(),
+			id: crypto.randomUUID(),
 			date: new Date(),
 			content,
-		}
+		};
 
-		setNotes([newNote, ...notes ]);
+		const notesArray = [newNote, ...notes];
+
+		setNotes(notesArray);
+
+		localStorage.setItem("notes", JSON.stringify(notesArray));
 	}
-	
+
 	return (
 		<div className="mx-auto max-w-6xl my-12 space-y-6">
 			<img src={logo} alt="NLW Expert" />
@@ -27,6 +49,7 @@ export function App() {
 					type="text"
 					placeholder="Busque em suas notas..."
 					className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+					onChange={handleSearch}
 				/>
 			</form>
 			<div className="h-px bg-slate-700" />
